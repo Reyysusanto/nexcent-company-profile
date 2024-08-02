2a8ee7e96614b20f828d4d76879a3ad77644c452
Reyysusanto <rheinaldysusanto@gmail.com>
2024-08-02 20:59:17 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#4CAF4F',
+        silver: '#94a3b8',
+      },
+    },
+  },
+  plugins: [],
+}
+
